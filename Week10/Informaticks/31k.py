2399f1e6242c016b6363ba79c9586cac1c01838d
EldarMT <tastanov.eldar00@mail.ru>
2019-04-09 15:29:41 +0600

    newweb

---
+++ Week10/Informaticks/31k.py
@@ -0,0 +1,7 @@
+    
+a = int(input())
+s = 0
+for i in range(1,a+1):
+    c = int(input())
+    s += c
+print(str(s))
---
+++ Week10/Informaticks/5b.py
@@ -0,0 +1,6 @@
+
+import math
+def f(a,n):
+    return pow(float(a),float(n))
+a, n = input().split()
+print(f(a,n))
