958636e2e15890363911db8289f859e845a096ed
EldarMT <35527767+EldarMT@users.noreply.github.com>
2019-04-09 15:32:24 +0600

    Update activate.ps1

--- Week11/Back-end/backenv/Scripts/activate.ps1
+++ Week11/Back-end/backenv/Scripts/activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\Zhanar\Desktop\Webka\week11\Back-End\backenv"
+
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
