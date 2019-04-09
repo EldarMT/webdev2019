b70cecfc352dbf40165628afab2900d36cf4a62e
EldarMT <35527767+EldarMT@users.noreply.github.com>
2019-04-09 15:31:46 +0600

    Update activate.bat

--- Week11/Back-end/backenv/Scripts/activate.bat
+++ Week11/Back-end/backenv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set "VIRTUAL_ENV=C:\Users\Zhanar\Desktop\Webka\week11\Back-End\backenv"
+
 
 if not defined PROMPT (
     set "PROMPT=$P$G"
